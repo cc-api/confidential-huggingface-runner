1464cbfb972c30d6c8813528d3043b0377fbaf19
James O. D. Hunt <james.o.hunt@intel.com>
2024-06-12 11:37:53 +0100

    build: Added info/error/die functions
    Made the output more consistent and simplified the error handling
    by introducing a `die()` function.
    
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

--- containers/build.sh
+++ containers/build.sh
@@ -13,6 +13,26 @@ docker_build_clean_param=""
 model_id="dongx1x/Llama-2-7b-chat-hf-sharded-bf16-aes"
 all_containers=()
 
+function info {
+    local msg="${1:-}"
+
+    echo -e "INFO: $msg"
+}
+
+function error {
+    local msg="${1:-}"
+
+    echo >&2 -e "ERROR: $msg"
+}
+
+function die {
+    local msg="${1:-}"
+
+    error "$msg"
+
+    exit 1
+}
+
 function scan_all_containers {
     mapfile -t dirs < <(cd "${container_directory}" && ls -d ./*/)
     for dir in "${dirs[@]}"
@@ -48,28 +68,27 @@ while getopts ":a:c:g:hm:p:r:" option; do
             m) model_id=${OPTARG};;
             p) repo=${OPTARG};;
             r) registry=${OPTARG};;
-            *) echo "Invalid option: -${OPTARG}" >&2
+            *) error "Invalid option: -${OPTARG}"
                usage
                ;;
         esac
     done
 
     if [[ ! "$action" =~ ^(build|download|publish|save|all)$ ]]; then
-        echo "invalid type: $action"
+        error "invalid type: $action"
         usage
     fi
 
     if [ "$container" != 'all' ]; then
         if [[ ! "${all_containers[*]}" =~ ${container} ]]; then
-            echo "invalid container name: $container"
+            error "invalid container name: $container"
             usage
         fi
     fi
 
     if [ -z "$registry" ]; then
         if [ -z "$EIP_REGISTRY" ]; then
-            echo "Error: Please specify your docker registry via -r <registry prefix> or set environment variable EIP_REGISTRY."
-            exit 1
+            die "Please specify your docker registry via -r <registry prefix> or set environment variable EIP_REGISTRY."
         else
             registry=$EIP_REGISTRY
         fi
@@ -78,11 +97,11 @@ while getopts ":a:c:g:hm:p:r:" option; do
 
 function build_a_image {
     local img_container=$1
-    echo "Build container image => ${registry}/${img_container}:${tag}"
+    info "Build container image => ${registry}/${img_container}:${tag}"
 
     if [ -f "${container_directory}/${img_container}/pre-build.sh" ]; then
-        echo "Execute pre build script at ${container_directory}/${img_container}/pre-build.sh"
-        "${container_directory}/${img_container}/pre-build.sh" || { echo 'Fail to execute pre-build.sh'; exit 1; }
+        info "Execute pre build script at ${container_directory}/${img_container}/pre-build.sh"
+        "${container_directory}/${img_container}/pre-build.sh" || { die 'Failed to execute pre-build.sh'; }
     fi
 
     docker_build_args=(
@@ -104,16 +123,16 @@ function build_a_image {
     fi
 
     pushd "${container_directory}/${img_container}"
-    echo $PWD
+    info "PWD: '$PWD'"
     docker build "${docker_build_args[@]}" || \
-        { echo "Fail to build docker ${registry}/${img_container}:${tag}"; exit 1; }
+        { die "Failed to build docker ${registry}/${img_container}:${tag}"; }
     popd
 
-    echo "Complete build image => ${registry}/${img_container}:${tag}"
+    info "Complete build image => ${registry}/${img_container}:${tag}"
 
     if [ -f "${container_directory}/${img_container}/post-build.sh" ]; then
-        echo "Execute post build script at ${container_directory}/${img_container}/post-build.sh"
-        "${container_directory}/${img_container}/post-build.sh" || { echo "Fail to execute post-build.sh"; exit 1; }
+        info "Execute post build script at ${container_directory}/${img_container}/post-build.sh"
+        "${container_directory}/${img_container}/post-build.sh" || { die "Failed to execute post-build.sh"; }
     fi
 
     echo -e "\n\n"
@@ -132,10 +151,10 @@ function build_images {
 
 function publish_a_image {
     local img_container=$1
-    echo "Publish container image: ${registry}/${img_container}:${tag} ..."
+    info "Publish container image: ${registry}/${img_container}:${tag} ..."
     docker push "${registry}/${img_container}:${tag}" || \
-        { echo "Fail to push docker ${registry}/${img_container}:${tag}"; exit 1; }
-    echo -e "Complete publish container image ${registry}/${img_container}:${tag} ...\n"
+        { die "Failed to push docker ${registry}/${img_container}:${tag}"; }
+    info "Complete publish container image ${registry}/${img_container}:${tag} ...\n"
 }
 
 function publish_images {
@@ -151,26 +170,26 @@ function publish_images {
 
 function download_a_image {
     local img_container=$1
-    echo "Download container image: ${registry}/${img_container}:${tag} ..."
+    info "Download container image: ${registry}/${img_container}:${tag} ..."
     crictl pull "${registry}/${img_container}:${tag}" || \
-        { echo "Fail to download images ${registry}/${img_container}:${tag}"; exit 1; }
-    echo -e "Complete download container image ${registry}/${img_container}:${tag} ...\n"
+        { die "Failed to download images ${registry}/${img_container}:${tag}"; }
+    info "Complete download container image ${registry}/${img_container}:${tag} ...\n"
 }
 
 function download_images {
     if [ "$container" = 'all' ]; then
         for img_container in "${all_containers[@]}"
         do
-	    download_a_image "$img_container"
+            download_a_image "$img_container"
         done
     else
-	download_a_image "$container"
+        download_a_image "$container"
     fi
 }
 
 function save_a_image {
     local img_container=$1
-    echo "Save container image ${registry}/${img_container}:${tag} => ${top_directory}/images/ ... "
+    info "Save container image ${registry}/${img_container}:${tag} => ${top_directory}/images/ ... "
     mkdir -p "${top_directory}/images/"
     docker save -o "${top_directory}/images/${img_container}-${tag}.tar" "${registry}/${img_container}:${tag}"
     docker save "${registry}/${img_container}:${tag}" | gzip > "${top_directory}/images/${img_container}-${tag}.tgz"
@@ -190,8 +209,7 @@ function save_images {
 function check_docker {
     if ! command -v docker &> /dev/null
     then
-        echo "Docker could not be found. Please install Docker."
-        exit
+        die "Docker could not be found. Please install Docker."
     fi
 }
 
@@ -199,14 +217,14 @@ check_docker
 scan_all_containers
 process_args "$@"
 
-echo ""
-echo "-------------------------"
-echo "action: ${action}"
-echo "container: ${container}"
-echo "tag: ${tag}"
-echo "registry: ${registry}"
-echo "-------------------------"
-echo ""
+info ""
+info "-------------------------"
+info "action: ${action}"
+info "container: ${container}"
+info "tag: ${tag}"
+info "registry: ${registry}"
+info "-------------------------"
+info ""
 
 if [[ "$action" =~ ^(build|all)$ ]]; then
     build_images
